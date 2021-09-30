69331748a5a12b306bb7d4b5b8445c85160dc292
FaceToKeyboard <fanno.chea@gmail.com>
2021-09-30 14:18:53 -0600

    skeleton of API routes

--- server/index.js
+++ server/index.js
@@ -7,6 +7,26 @@ app.get('/', (req, res) => {
   res.status(200).send('This is the endpoint for review data');
 });
 
+app.get('/reviews', (req, res) => {
+  // TODO: function that will get and shape data from DB
+});
+
+app.get('/revews/meta', (req, res) => {
+  // TODO
+});
+
+app.post('/reviews', (req, res) => {
+  // TODO
+});
+
+app.put('/reviews/:review_id/helpful', (req) => {
+  const reviewId = req.params.review_id;
+});
+
+app.put('/reviews/:review_id/report', () => {
+
+});
+
 app.listen(port, () => {
   console.log('== Running on port ', port, '! ==');
   console.log('🚀🚀🚀🚀🚀🚀');

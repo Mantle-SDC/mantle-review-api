e54c0bc8030ddeddb14b742b6c24f2c7b4370582
FaceToKeyboard <fanno.chea@gmail.com>
2021-09-30 18:32:57 -0600

    establish db connection

--- database/index.js
+++ database/index.js
@@ -1,23 +1,2 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localost:27017/mantle');
-
-const reviewSchema = new mongoose.Schema({
-  id: Number,
-  rating: Number,
-  summary: String,
-  recommend: Boolean,
-  response: String,
-  body: String,
-  date: Date,
-  reviewer_name: String,
-  helpfulness: Number,
-  photos: [{
-    id: Number, url: String,
-  }],
-});
-
-// Name of the model, the schema to use, and - optionally - the exact name of the collection
-// to use in the database.
-const ReviewModel = mongoose.model('Review', reviewSchema, 'reviews');
-export default ReviewModel;
+require('./initDB');
+module.exports.getReviews = require('./getReviews');

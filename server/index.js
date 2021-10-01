bbd815b01b37afe16b87449e811b7ecf2e0e4dee
FaceToKeyboard <fanno.chea@gmail.com>
2021-09-30 18:27:38 -0600

    placeholders for API endpoints

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { getReviews } = require('../database/index');
 
 const app = express();
 const port = 3000;
@@ -9,22 +10,31 @@ app.get('/', (req, res) => {
 
 app.get('/reviews', (req, res) => {
   // TODO: function that will get and shape data from DB
+  getReviews(req.params.product_id)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => console.log('Error retrieving reviews from DB: ', err));
 });
 
 app.get('/revews/meta', (req, res) => {
   // TODO
+  res.status(200).send('This is the GET /reviews/meta endpoint');
 });
 
 app.post('/reviews', (req, res) => {
   // TODO
+  res.status(200).send('This is the POST /reviews/meta endpoint');
 });
 
-app.put('/reviews/:review_id/helpful', (req) => {
-  const reviewId = req.params.review_id;
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  // TODO
+  res.status(200).send('I received review_id ', req.params.review_id);
 });
 
-app.put('/reviews/:review_id/report', () => {
-
+app.put('/reviews/:review_id/report', (req, res) => {
+  // TODO
+  res.status(200).send('I received review_id ', req.params.review_id);
 });
 
 app.listen(port, () => {

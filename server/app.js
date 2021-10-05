883add007f617b09cf4efb005abb6499f2485bc5
FaceToKeyboard <fanno.chea@gmail.com>
2021-10-05 13:26:49 -0600

    linting

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { getReviews, getReviewsMeta, markHelpful, reportReview } = require('../database/index');
+const {
+  getReviews,
+  getReviewsMeta,
+  markHelpful,
+  reportReview,
+} = require('../database/index');
 
 const app = express();
 

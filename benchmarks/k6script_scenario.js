7573776a598a62d73ec7d03b3678b94d756f48a9
FaceToKeyboard <fanno.chea@gmail.com>
2021-10-07 11:39:35 -0600

    benchmark/performance tests we'll stick to

--- benchmarks/k6script_scenario.js
+++ benchmarks/k6script_scenario.js
@@ -8,19 +8,18 @@ export const options = {
       rate: 1000,
       timeUnit: '1s',
       duration: '30s',
-      preAllocatedVUs: 2000,
-      maxVUs: 4000,
+      preAllocatedVUs: 1000,
+      maxVUs: 1200,
     },
   },
   thresholds: {
-    http_req_duration: ['p(95)<50'],
+    http_req_duration: ['p(99)<50'],
     http_req_failed: ['rate<0.01'],
   },
 };
 
 export default () => {
   group('GET /reviews', () => {
-    http.get('http://localhost:3000/reviews?product_id=100');
-    http.get('http://localhost:3000/reviews/meta?product_id=100');
+    http.get('http://localhost:3000/reviews?product_id=12');
   });
 };
